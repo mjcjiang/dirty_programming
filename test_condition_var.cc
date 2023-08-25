98c0b261a77cb8e05bed611cb2ccd41b8d15f75d
mjcjiang <redmagic039@gmail.com>
2023-08-25 22:22:34 +0800

    commit

--- test_condition_var.cc
+++ test_condition_var.cc
@@ -10,15 +10,20 @@ std::condition_variable data_cond;
 std::queue<int> data_queue;
 
 void data_preparation_thread() {
-    std::lock_guard<std::mutex> lk(mut);
-    data_queue.push(10);
-    std::cout << "finish push" << std::endl;
-    data_cond.notify_one();
+    {
+        std::lock_guard<std::mutex> lk(mut);
+        data_queue.push(10);
+        std::cout << "finish push" << std::endl;
+        std::this_thread::sleep_for(std::chrono::seconds(10));
+        data_cond.notify_one();
+    }
 }
 
 void data_processing_thread() {
-    std::unique_lock<std::mutex> lk{mut, std::defer_lock};
-    data_cond.wait(lk, []{return !data_queue.empty();});
+    std::unique_lock<std::mutex> lk{mut};
+    data_cond.wait(lk, []{
+        std::cout << "hello" << std::endl;
+        return !data_queue.empty();});
     int data = data_queue.front();
     data_queue.pop();
     lk.unlock();
@@ -27,7 +32,7 @@ void data_processing_thread() {
 
 int main(int argc, char *argv[])
 {
-    std::thread t1(data_processing_thread);
+    std::thread t1(data_preparation_thread);
     std::thread t2(data_processing_thread);
     t1.join();
     t2.join();
