2d0acd5d440fd4f08c63755f495a2bccf9091332
mjcjiang <redmagic039@gmail.com>
2023-08-25 21:27:13 +0800

    code commit

---
+++ test_asm.s
@@ -0,0 +1,42 @@
+	.arch armv6
+	.eabi_attribute 28, 1
+	.eabi_attribute 20, 1
+	.eabi_attribute 21, 1
+	.eabi_attribute 23, 3
+	.eabi_attribute 24, 1
+	.eabi_attribute 25, 1
+	.eabi_attribute 26, 2
+	.eabi_attribute 30, 6
+	.eabi_attribute 34, 1
+	.eabi_attribute 18, 4
+	.file	"test_asm.cc"
+	.text
+	.align	2
+	.global	_Z9test_funci
+	.arch armv6
+	.syntax unified
+	.arm
+	.fpu vfp
+	.type	_Z9test_funci, %function
+_Z9test_funci:
+	.fnstart
+.LFB0:
+	@ args = 0, pretend = 0, frame = 8
+	@ frame_needed = 1, uses_anonymous_args = 0
+	@ link register save eliminated.
+	str	fp, [sp, #-4]!
+	add	fp, sp, #0
+	sub	sp, sp, #12
+	str	r0, [fp, #-8]
+	ldr	r3, [fp, #-8]
+	add	r3, r3, #1
+	mov	r0, r3
+	add	sp, fp, #0
+	@ sp needed
+	ldr	fp, [sp], #4
+	bx	lr
+	.cantunwind
+	.fnend
+	.size	_Z9test_funci, .-_Z9test_funci
+	.ident	"GCC: (Raspbian 10.2.1-6+rpi1) 10.2.1 20210110"
+	.section	.note.GNU-stack,"",%progbits
